8d1420a7ee8e03eebaffedad867018e55cfedf50
eldorado-22 <jumashevv992@gmail.com>
2023-04-24 16:21:01 +0600

    movies-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
+import {Route, Routes} from "react-router-dom";
+import Popular from "./pages/Popular/Popular";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Routes>
+                <Route path={"/popular"} element={<Popular/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header id="header">
+
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="#" className="flex items-center">
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"/>
+                        <span
+                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Movies</span>
+                    </a>
+
+                    <div className="flex md:order-2">
+                        <button type="button"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Dark
+                        </button>
+                    </div>
+
+                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                         id="navbar-cta">
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <NavLink to={""}
+                                     className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                     aria-current="page">Home</NavLink>
+                            <NavLink to={"/popular"}
+                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                     aria-current="page">Popular
+                            </NavLink>
+                            <NavLink to={""}
+                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Top Rated
+                            </NavLink>
+                            <NavLink to={""}
+                                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Now Playing
+                            </NavLink>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+        </header>
+    );
+};
+
+export default Header;
