4dbcc53ad536d160c6f15acf15be5e29d2daf4a3
eldorado-22 <jumashevv992@gmail.com>
2024-02-13 15:58:07 +0600

    movies_new_branch

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -2,3 +2,6 @@ import {useDispatch} from "react-redux";
 import {AppDispatch} from "../Reducer/redux/store";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+
+// useAppDispatch
---
+++ src/pages/Homes/homes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from "./Hero/Hero";
+
+const Homes = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    );
+};
+
+export default Homes;
